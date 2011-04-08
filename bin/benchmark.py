914b03e27fd4c544fb1d49631420661462330900
Dave Hall <david@etianen.com>
2011-04-08 15:51:01 +0100

    Added in benchmark for performance comparisons

--- bin/benchmark.py
+++ bin/benchmark.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
 
 import moody, os.path
+import timeit
 
 
 def main():
     print("Running benchmarks for moody-templates...")
-    loader = moody.make_loader(os.path.join(os.path.dirname(__file__), "templates"))
-    print(loader.render("index.html",
-        list_items = (
-            "Foo",
-            "Bar",
-        )
-    ))
+    template_root = os.path.join(os.path.dirname(__file__), "templates")
+    def benchmark(func):
+        return min(timeit.repeat(func, repeat=3, number=1000))
+    # Test the cached rendering.
+    loader = moody.make_loader(template_root)
+    def render_cached():
+        loader.render("index.html", list_items = list(range(100)))
+    print("Cached rendering:   {time}".format(time=benchmark(render_cached)))
+    # Test the uncached rendering.
+    def render_uncached():
+        loader._cache.clear()
+        loader.render("index.html", list_items = list(range(100)))
+    print("Uncached rendering: {time}".format(time=benchmark(render_uncached)))
     
     
 if __name__ == "__main__":
